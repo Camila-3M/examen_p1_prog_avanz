7b79b7476660f51a0ead52f2dd3af4117af89c00
Bssss <cmaldonado6e@gmail.com>
2026-05-28 20:19:29 -0500

    subida del proyecto

---
+++ src/main/java/com/exa_parcial/EXA_Parcial/ExaParcialApplication.java
@@ -0,0 +1,13 @@
+package com.exa_parcial.EXA_Parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exa_parcial/EXA_Parcial/ExaParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exa_parcial.EXA_Parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
